96949f826ab758397660458b9aad0b93eb80dad6
RAY\tohya <tohyama@psof.co.jp>
2018-05-17 15:04:51 +0900

    'add'

--- hooks/application_start.sh
+++ hooks/application_start.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
  
-app_src_path="/usr/src/app"
-app_log_path="/var/log/app"
+app_path="/usr/src/app/bin/www"
+forever_log="/var/log/app/forever.log"
+app_access_log="/var/log/app/access.log"
+app_error_log="/var/log/app/error.log"
  
-mkdir "$app_src_path" "$app_log_path"
+forever start \
+  -l "$forever_log" \
+  -o "$app_access_log" \
+  -e "$app_error_log" \
+  "$app_path"
